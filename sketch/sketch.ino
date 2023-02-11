94c602e2309747791224002657c3f3fde746b1fc
ctrlouis <lo.bertschy@gmail.com>
2023-02-12 00:46:39 +0100

    Record with push buton

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -22,7 +22,10 @@ HttpClient influxClient = HttpClient(wifi, SECRET_INFLUXDB_ADDRESS, SECRET_INFLU
 InfluxBucket arduinoBucket = InfluxBucket(SECRET_INFLUXDB_ORG, SECRET_INFLUXDB_BUCKET, SECRET_INFLUXDB_TOKEN, "s");
 
 // led setup
-Led ledWifi = Led(0, OUTPUT, LOW);
+Led pauseLed = Led(0, OUTPUT, LOW);
+Led recordLed = Led(1, OUTPUT, LOW);
+Led wifiLed = Led(2, OUTPUT, LOW);
+Led button = Led(3, INPUT);
 
 // temperature sensor setup
 TemperatureSensor sensors[1] = {
@@ -30,9 +33,13 @@ TemperatureSensor sensors[1] = {
 };
 const int sensorsSize = sizeof(sensors) / sizeof(sensors[0]);
 
+bool buttonReleased = true, record = false, recordChange = false;
+int buttonPressed, stopCounter = 0;
+
 void setup() {
   bool dbAvailable = false;
-
+  startBlink();
+  pauseLed.high();
   analogReadResolution(ANALOG_READ_RESOLUTION);
   Serial.begin(9600);
   while (!Serial);
@@ -42,17 +49,71 @@ void setup() {
   while (!dbAvailable) {
     dbAvailable = arduinoBucket.testConnection(influxClient);
   }
-  ledWifi.high();
+  wifiLed.high();
 }
 
 void loop() {
-  for (int i = 0; i < sensorsSize; i++) {
-    sensors[i].readTemperature();
-    Serial.println(sensors[i].toString());
-    arduinoBucket.addRow("temperatureSensors", sensors[i].getPin(), sensors[i].getLocation(), sensors[i].getTemperature());
-    arduinoBucket.write(influxClient, true);
-    arduinoBucket.clearRow();
+  buttonPressed = button.read();
+
+  if (buttonPressed == HIGH) {
+    buttonReleased = false;
+    if (!recordChange && !record) {
+      record = true;
+      recordChange = true;
+      pauseLed.low();
+      recordLed.high();
+    } else if (!recordChange && !record) {
+      stopCounter++;
+    }
+    if (stopCounter > 4) {
+        record = false;
+        recordChange = true;
+        pauseLed.high();
+        recordLed.low();
+    }
+  } else if (buttonPressed == LOW) {
+    buttonReleased = true;
+    recordChange = false;
+  }
+
+  if (record) {
+    for (int i = 0; i < sensorsSize; i++) {
+      sensors[i].readTemperature();
+      Serial.println(sensors[i].toString());
+      arduinoBucket.addRow("temperatureSensors", sensors[i].getPin(), sensors[i].getLocation(), sensors[i].getTemperature());
+      arduinoBucket.write(influxClient);
+      arduinoBucket.clearRow();
+    }    
   }
   delay(500);
 }
 
+void startBlink() {
+  int duration = 300;
+  pauseLed.high();
+  recordLed.low();
+  wifiLed.low();
+  delay(duration);
+  pauseLed.low();
+  recordLed.high();
+  wifiLed.low();
+  delay(duration);
+  pauseLed.low();
+  recordLed.low();
+  wifiLed.high();
+  delay(duration);
+  for(int i = 0; i < 3; i++) {
+    pauseLed.low();
+    recordLed.low();
+    wifiLed.low();
+    delay(duration);
+    pauseLed.high();
+    recordLed.high();
+    wifiLed.high();
+    delay(duration);
+  }
+  pauseLed.low();
+  recordLed.low();
+  wifiLed.low();
+  delay(duration);
+}
